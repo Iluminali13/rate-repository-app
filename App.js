ab221ed64da55b472bcb932119a65c85dcd5c073
iluminali13 <pettinarolijuan06@gmail.com>
2023-08-29 11:59:00 -0300

    segundo subida

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react"
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>hola capo como estas  </Text>
+      <Image 
+        source={{uri: 'https://picsum.photos/200/300'}}
+        style={styles.image}     
+      />
     </View>
   );
-
 };
 
 //objeto y atributo, llamar al objeto ej styles. y su respectivo atributo container: styles.container
@@ -22,7 +25,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: 'green' 
   },
-
+  image: {
+    weight: 200, 
+    width: 200
+  }
 })
 
 
